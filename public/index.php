7a99c78b0c969bf2b5e394ce45d973cb495a4eaa
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-02-20 10:30:57 +0100

    init du index.php dans le dossier public

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+// Chargement de l'autoloader créé par composer (situé dans le sous dossier "vendor")
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+// chargement des variables d'environnement à partir du dossier racine
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$uri = $_SERVER['REQUEST_URI'];
+
+var_dump($uri);
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit551a4839db94ef72258068a27c404e7a
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit551a4839db94ef72258068a27c404e7a', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit551a4839db94ef72258068a27c404e7a', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit551a4839db94ef72258068a27c404e7a
         call_user_func(\Composer\Autoload\ComposerStaticInit551a4839db94ef72258068a27c404e7a::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit551a4839db94ef72258068a27c404e7a::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
